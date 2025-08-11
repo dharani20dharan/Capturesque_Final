2ac12f7407c1856c96ddc20ba928851bcccf2bd9
dharani20dharan <dharani080905@gmail.com>
2025-08-11 17:42:04 +0530

    Home page Frontend Done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Intro from "../components/Intro";
+import FeaturedPhotos from "../components/Featuredphotos";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Intro />
+      <FeaturedPhotos />
+    </>
+  );
+};
+
+export default Home;
