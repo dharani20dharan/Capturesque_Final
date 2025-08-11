a66a63d14373f63d3f336362a02aad48174ee03f
dharani20dharan <dharani080905@gmail.com>
2025-08-11 17:43:39 +0530

    ClubInfo page skeleton

---
+++ src/pages/ClubInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ClubInfo = () => {
+  return (
+    <div>
+      <h1>About the Club</h1>
+      <p>Here’s everything you need to know about our club events and activities.</p>
+    </div>
+  );
+};
+
+export default ClubInfo; // <-- This is the fix
