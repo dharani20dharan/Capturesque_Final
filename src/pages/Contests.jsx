85558838be5c69663e95c58c7cb9037792de41dc
dharani20dharan <dharani080905@gmail.com>
2025-08-11 17:43:12 +0530

    Contests page skeleton

---
+++ src/pages/Contests.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contests = () => {
+  return (
+    <div>
+      <h1>Contests</h1>
+      <p>Stay tuned for upcoming photography contests and challenges.</p>
+    </div>
+  );
+};
+
+export default Contests;
