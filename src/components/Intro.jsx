9e7583405c24586d0a33efd4e558a78d5184546c
dharani20dharan <dharani080905@gmail.com>
2025-09-01 00:24:49 +0530

    Intro updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,33 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { FaSignInAlt } from 'react-icons/fa'; // Login icon
+import { FaSignInAlt } from 'react-icons/fa'; // Import login icon
 import "./Intro.css";
 
 const Intro = () => {
   return (
     <section className="intro">
-      {/* Background overlay (styling handled in CSS) */}
+      {/* Optional: Add pseudo-elements for background effects via CSS */}
       <div className="intro-background-overlay"></div>
 
-      {/* Main content block */}
       <div className="intro-content">
         <h2 className="intro-heading">
           Unlock Your Photos with Intelligent Recognition
         </h2>
-
-        {/* Description of the AI face recognition feature */}
         <p className="intro-text">
           Experience the future of photo organization. Our cutting-edge <span className="highlight-term">AI Face Recognition</span> automatically tags and sorts club event photos, making discovery effortless.
         </p>
-
-        {/* Call-to-action highlighting login benefits */}
         <p className="intro-highlight">
           Log in to access your personalized gallery, view tagged images, and securely download your moments.
         </p>
-
-        {/* Login button with icon */}
         <Link to="/Login" className="btn-login">
-          <FaSignInAlt className="btn-icon" />
+          <FaSignInAlt className="btn-icon" /> {/* Add icon */}
           Access Your Gallery
         </Link>
       </div>
